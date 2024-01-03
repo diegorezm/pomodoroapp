b9483e494cf0246f5c681636c2d7b9e4cb50845b
diegorezm <dmourarezende@gmail.com>
2024-01-03 14:13:29 -0300

    pomodoro logic

--- src/components/PomodoroTimer/index.tsx
+++ src/components/PomodoroTimer/index.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useCallback } from "react";
 import { useInterval } from "../../hooks/useInterval";
 import Button from "../Button";
 import Timer from "../Timer";
@@ -16,15 +16,19 @@ export default function PomodoroTimer(props: Props) {
   const [working, setWorking] = useState(false);
   const [resting, setResting] = useState(false);
   const [countingTime, setCountingTime] = useState(false);
+  const [cycles, setCycles] = useState(0);
+  const [cyclesQTD, setCyclesQTD] = useState(new Array(props.numberOfcycles - 1).fill(true));
+  const [numberOfPomodoros, setNumberOfPomodoros] = useState(0);
+  const [workingTime, setWorkingTime] = useState(0);
 
-  const configureWork = () => {
+  const configureWork = useCallback(() => {
     setWorking(true);
     setResting(false);
     setCountingTime(true);
     setMainTime(props.defaultPomodoroTime);
-  };
+  }, [props.defaultPomodoroTime]);
 
-  const configureResting = (long: boolean) => {
+  const configureResting = useCallback((long: boolean) => {
     setResting(true);
     setWorking(false);
     setCountingTime(true);
@@ -33,7 +37,7 @@ export default function PomodoroTimer(props: Props) {
     } else {
       setMainTime(props.defaultShortRestingTime);
     }
-  };
+  }, [props.defaultLongRestingTime, props.defaultShortRestingTime]);
 
   const playPause = () => {
     setCountingTime(!countingTime);
@@ -42,17 +46,50 @@ export default function PomodoroTimer(props: Props) {
   useEffect(() => {
     if (working) document.body.classList.add("working");
     if (resting) document.body.classList.remove("working");
-  }, [working, resting]);
+
+    if (mainTime > 0) return;
+
+    if (working && cyclesQTD.length > 0) {
+      configureResting(false);
+      cyclesQTD.pop();
+    } else if (working && cyclesQTD.length <= 0) {
+      setCyclesQTD(new Array(props.numberOfcycles - 1).fill(true));
+      configureResting(true);
+      setCycles(cycles + 1);
+    }
+    if (working) setNumberOfPomodoros(numberOfPomodoros + 1);
+    if (resting) configureWork();
+  }, [working, resting, mainTime, cyclesQTD, numberOfPomodoros, configureWork, configureResting, props.numberOfcycles, cycles]);
 
   useInterval(() => {
-
+    setMainTime(mainTime - 1);
+    if (working) {
+      setWorkingTime(workingTime + 1);
+    }
   }, countingTime ? 1000 : null);
 
   return (
     <div className="pomodoro">
       <h2>You are: {working ? "working" : "resting"}</h2>
+
+      <Timer time={mainTime} />
       <div className="buttons">
-        
+        <Button text="work" onClick={configureWork} className="default" />
+        <Button text="rest" onClick={() => configureResting(false)} className="default" />
+        <Button text={countingTime ? "pause" : "resume"} onClick={playPause} className={!working && !resting ? "hidden" : "default"} />
+      </div>
+      <div className="info__panel">
+        <p>
+          cycles: {cycles}
+        </p>
+
+        <p>
+          Total working time: {secondsToTime(workingTime)}
+        </p>
+
+        <p>
+          Time blocks: {numberOfPomodoros}
+        </p>
       </div>
     </div>
   );
