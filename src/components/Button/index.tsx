50d933fc3bdbc84300385d2c9afff8bcb1546a9f
diegorezm <dmourarezende@gmail.com>
2024-01-03 13:26:06 -0300

    default buttons

---
+++ src/components/Button/index.tsx
@@ -0,0 +1,15 @@
+import  './style.css';
+
+interface Props {
+  className: 'danger' | 'default',
+  onClick: () => void,
+  text: string
+}
+
+export default function Button(props: Props) {
+  return (
+    <button className={`button ${props.className}`} onClick={props.onClick}>
+      {props.text}
+    </button>
+  );
+}
