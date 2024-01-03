840dea8a703827f960e0c559099db07751ca354e
diegorezm <dmourarezende@gmail.com>
2024-01-03 13:45:01 -0300

    refactor

--- src/components/PomodoroTimer/index.tsx
+++ src/components/PomodoroTimer/index.tsx
@@ -13,95 +13,47 @@ interface Props {
 }
 export default function PomodoroTimer(props: Props) {
   const [mainTime, setMainTime] = useState(props.defaultPomodoroTime);
-  const [interval, setInterval] = useState<number | null>(null);
-  const [totalWorkingTime, setTotalWorkingTime] = useState(0);
-  const [timeBlocks, setTimeBlocks] = useState(0);
-  const [restingTime, setRestingTime] = useState(props.defaultShortRestingTime);
-  const [cycles, setCycles] = useState(0);
   const [working, setWorking] = useState(false);
+  const [resting, setResting] = useState(false);
+  const [countingTime, setCountingTime] = useState(false);
+
+  const configureWork = () => {
+    setWorking(true);
+    setResting(false);
+    setCountingTime(true);
+    setMainTime(props.defaultPomodoroTime);
+  };
+
+  const configureResting = (long: boolean) => {
+    setResting(true);
+    setWorking(false);
+    setCountingTime(true);
+    if (long) {
+      setMainTime(props.defaultLongRestingTime);
+    } else {
+      setMainTime(props.defaultShortRestingTime);
+    }
+  };
+
+  const playPause = () => {
+    setCountingTime(!countingTime);
+  };
 
   useEffect(() => {
-    if (working) {
-      document.body.classList.add('working');
-    }
-    else {
-      document.body.classList.remove('working');
-    }
-  }, [working]);
-
-  const stopTimer = () => {
-    setMainTime(0);
-    setInterval(null);
-    setTimeBlocks(0);
-    setCycles(0);
-    setTotalWorkingTime(0);
-    setWorking(false);
-  };
-
-  const startTimer = () => {
-    setMainTime(props.defaultPomodoroTime);
-    setInterval(1000);
-    setWorking(true);
-  };
-
-  const pauseResumeTimer = () => {
-    if (interval) {
-      setInterval(null);
-    } else {
-      setInterval(1000);
-    }
-  };
+    if (working) document.body.classList.add("working");
+    if (resting) document.body.classList.remove("working");
+  }, [working, resting]);
 
   useInterval(() => {
-    if (working) {
-      setMainTime(mainTime - 1);
-      setTotalWorkingTime(totalWorkingTime + 1);
-      if (mainTime === 0) {
-        setTimeBlocks(timeBlocks + 1);
-        if (cycles === props.numberOfcycles) {
-          setRestingTime(props.defaultLongRestingTime);
-        }
-        setMainTime(props.defaultPomodoroTime);
-        setWorking(false);
-      }
-    }
-    else {
-      setRestingTime(restingTime - 1);
-      if (restingTime === 0) {
-        setMainTime(props.defaultPomodoroTime);
-        setRestingTime(props.defaultShortRestingTime);
-        setCycles(cycles + 1);
-        setWorking(true);
-      }
-    }
-  }, interval);
+
+  }, countingTime ? 1000 : null);
 
   return (
     <div className="pomodoro">
-      <h2>
-        You are: {working ? "working" : "resting"}
-      </h2>
-      <Timer time={working ? mainTime : restingTime} />
-
+      <h2>You are: {working ? "working" : "resting"}</h2>
       <div className="buttons">
-        <Button text="start" onClick={startTimer} className="default" />
-        <Button text="stop" onClick={stopTimer} className="danger" />
-        <Button text={interval ? "pause" : "resume"} onClick={pauseResumeTimer} className="default" />
+        
       </div>
-      <div className="info__panel">
-        <p>
-          cycles: {cycles}
-        </p>
-
-        <p>
-          Total working time: {secondsToTime(totalWorkingTime)}
-        </p>
-
-        <p>
-          Time blocks: {timeBlocks}
-        </p>
-      </div>
-
     </div>
   );
 }
